5e6e302a532025e18b21a945a0c23559d01ede48
dacx910 <62887679+dacx910@users.noreply.github.com>
2024-12-07 18:03:45 -0500

    day5 part 2 (not finished)

--- day5/day5.cpp
+++ day5/day5.cpp
@@ -1,10 +1,37 @@
 #include <fstream>
+#include <vector>
+#include <string>
+#include <iostream>
+
+bool checkIsValid(std::string input) {
+
+}
 
 int main() {
-    std::fstream rules("rules.txt");
-    std::fstream pages("pages.txt");
+    std::fstream inputRules("rules.txt");
+    std::fstream inputPages("pages.txt");
+    std::vector<std::vector<int>> rules(100);
+    std::string input;
 
-    
+    if (!inputRules.is_open() || !inputPages.is_open()) {
+        std::cout << "File open failed\n";
+        return -1;
+    }
+
+    // Read rules
+    while (getline(inputRules, input)) {
+        int num = stoi(input.substr(0,2));
+        int preceding = stoi(input.substr(3,2));
+
+        rules.at(num).push_back(preceding);
+    }
+
+    // Read pages
+    while (getline(inputPages, input)) {
+        if (checkIsValid(input)) {
+            std::cout << input;
+        }
+    }
 
     return 0;
 }
